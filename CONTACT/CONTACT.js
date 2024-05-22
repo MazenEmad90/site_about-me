b9b138f51541bb1044771b5cdceb9e0907061406
Mazen Emad <150241042+MazenEmad90@users.noreply.github.com>
2024-05-22 14:58:51 +0300

    Add files via upload

---
+++ CONTACT/CONTACT.js
@@ -0,0 +1,20 @@
+let mm = document.getElementsByClassName("mm")[0];
+
+red.onclick = function() {
+    mm.style.backgroundColor = "red";
+};
+green.onclick= function(){
+    mm.style.backgroundColor="green";
+}
+yellow.onclick = function() {
+    mm.style.backgroundColor = "yellow";
+};
+blue.onclick= function(){
+    mm.style.backgroundColor="blue";
+}
+orange.onclick = function() {
+    mm.style.backgroundColor = "orange";
+};
+normal.onclick= function(){
+    mm.style.backgroundColor="#191a1a";
+}
